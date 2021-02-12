d705d30baf6df2977fcd837c8265456d5a1fc6d4
Martin Coplák <martin.coplak@gmail.com>
2021-02-12 11:58:57 +0100

    Initial commit, start of development

---
+++ src/main/java/eu/trenend/minesweeper/Minesweeper.java
@@ -0,0 +1,15 @@
+package eu.trenend.minesweeper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+import org.bukkit.plugin.java.annotation.plugin.author.Author;
+
+@Author("misuda12")
+@Plugin(name = "Minesweeper", version = "1.0-SNAPSHOT")
+public class Minesweeper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
---
+++ src/main/java/eu/trenend/minesweeper/model/package-info.java
@@ -0,0 +1,1 @@
+package eu.trenend.minesweeper.model;
